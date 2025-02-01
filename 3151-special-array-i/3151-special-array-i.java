7fe8cd25857396cb4b9ad723e91a334d32d1a2dc
Om3101 <119741686+Om3101@users.noreply.github.com>
2025-02-02 02:13:19 +0530

    Time: 0 ms (100%), Space: 44 MB (8.84%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.java
+++ 3151-special-array-i/3151-special-array-i.java
@@ -4,6 +4,8 @@ class Solution
     {
         int n = nums.length;
 
+        if(n==1)   return true;
+
         for(int i=0; i<n-1; i++)
         {
             if ((nums[i] % 2) == (nums[i + 1] % 2))  return false;
