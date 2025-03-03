46316589b9d99699c2b66e3f9b9d095d89a1abf7
Om3101 <119741686+Om3101@users.noreply.github.com>
2025-03-04 00:20:51 +0530

    Time: 5 ms (85.39%), Space: 68.2 MB (35.78%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,32 +1,24 @@
-/* intuition is to maintain 3 diff lists for <,=,> elements
-    and fill the values in them and then merge all the 3
-    as we cannot do inplace partitioning as*/
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] result = new int[n];  // Temporary array for result
+        int left = 0, right = 0;    
 
+        // First pass: Place elements smaller than pivot
+        for (int num : nums) {
+            if (num < pivot) result[left++] = num;
+        }
 
-class Solution
-{
-    public int[] pivotArray(int[] nums, int pivot)
-    {
-       ArrayList<Integer> a1 = new ArrayList<>();
-       ArrayList<Integer> a2 = new ArrayList<>();
-       ArrayList<Integer> a3 = new ArrayList<>();
+        // Second pass: Place elements equal to pivot
+        for (int num : nums) {
+            if (num == pivot) result[left++] = num;
+        }
 
-       for(int num: nums)
-       {
-          if(num<pivot)    a1.add(num);
+        // Third pass: Place elements greater than pivot
+        for (int num : nums) {
+            if (num > pivot) result[left++] = num;
+        }
 
-          else if (num==pivot)  a2.add(num);
-
-          else  a3.add(num);
-       }
-
-        //merge
-
-        int i=0;
-        for(int num:a1)     nums[i++] = num;
-        for(int num:a2)     nums[i++] = num;
-        for(int num:a3)     nums[i++] = num;
-
-        return nums;
+        return result; // Return the rearranged array
     }
 }
