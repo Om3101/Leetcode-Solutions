4a44c0acdc213e094b8aab6351fda0c92e579323
Om3101 <119741686+Om3101@users.noreply.github.com>
2025-03-11 21:21:22 +0530

    1358-number-of-substrings-containing-all-three-characters.java

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -11,7 +11,8 @@ class Solution
             lastSeen[s.charAt(i) - 'a'] = i; // Update last seen index
 
             // Check if all three characters have been seen
-            if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
+            if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1)
+            {
                 count += 1 + Math.min(lastSeen[0], Math.min(lastSeen[1], lastSeen[2]));
             }
         }
